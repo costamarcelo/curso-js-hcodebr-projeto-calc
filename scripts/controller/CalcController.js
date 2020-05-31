efd826d89594f9c3f7e253bdb9a6b5c922d7e53a
Marcelo Costa <costamarcelo.ti@gmail.com>
2020-05-31 15:18:17 -0300

    Mouseover, mouseup and mousedown

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -14,41 +14,32 @@ class CalcController {
     }
 
     initialize(){
-
         this.setDisplayDateTime();
-
         setInterval(() => {
-
             this.setDisplayDateTime();
-
         }, 1000);
-
     }
 
     addEventListenerAll(element, events, fn){
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
-
         })
     }
 
 
     initButtonEvents(){
-
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
-
         buttons.forEach((btn, index)=>{
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal);
+                console.log(btn.className.baseVal.replace('btn-', ''));
             });
-            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e =>{
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
                 btn.style.cursor = 'pointer';
             });
         });
     }
 
     setDisplayDateTime(){
-
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
@@ -58,50 +49,31 @@ class CalcController {
     }
 
     get displayCalc(){
-
         return this._displayCalcEl.innerHTML;
     }
-
     set displayCalc(value){
-
         this._displayCalcEl.innerHTML = value;
-
     }
 
     get displayDate(){
-
         return this._dateEl.innerHTML;
-
     }
-
     set displayDate(value){
-
         this._dateEl.innerHTML = value;
-
     }
 
     get displayTime(){
-
         return this._timeEl.innerHTML;
-
     }
-
     set displayTime(value){
-
         this._timeEl.innerHTML = value;
-
     }
 
 
     get currentDate(){
-
         return new Date();
-
     }
-
     set currentDate(value){
-
         this._currentDate = value;
     }
-
 }
